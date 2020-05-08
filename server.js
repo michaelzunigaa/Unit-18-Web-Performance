3d1402b53d1c8b3357129956b20c9b5650fd8e30
michael Zuniga <michaelzunigaa@gmail.com>
2020-05-08 10:45:21 -0700

    deploy-fix

--- server.js
+++ server.js
@@ -15,14 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODDB_URI =  process.env.MONGODDB_URI || "monodb://locahost/mongoHeadlines";
+var MONGODB_URI =  process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 
-mongoose.connect(MONGOD_URI);
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false
-});
+  useUnifiedTopology: true
+}).catch(err => console.log(err));
+mongoose.set("useFindAndModify", false)
 
 // routes
 app.use(require("./routes/api.js"));
